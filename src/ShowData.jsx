822b64927afee98662bcb4e103b343df7f94822e
Your Name <tahsin.mail.0503@gmail.com>
2024-03-10 16:41:02 +0600

    add buttom

--- src/ShowData.jsx
+++ src/ShowData.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import "./ShowData1.css";
 
 const ShowData = ({ country }) => {
-  console.log(country.flags.png);
+  console.log(country);
   const image = country.flags.png;
   const imageUrl = country.flags.png;
   return (
@@ -10,8 +10,10 @@ const ShowData = ({ country }) => {
       <div className="mainDiv">
         <img className="image" src={imageUrl} />
         <h1>{country.name.common}</h1>
-        <h1>Capital: {country.capital}</h1>
-        <a href="{country.flags.png}">dd</a>
+        <h2>{country.name.official}</h2>
+        <h3>Capital: {country.capital}</h3>
+        <button onClick={ShowDeatils} >Show Details</button>
+        {/* <a href="{country.flags.png}">dd</a> */}
         {/* <a>{country.flags.png}</a>  */}
       </div>
     </div>
@@ -19,3 +21,7 @@ const ShowData = ({ country }) => {
 };
 
 export default ShowData;
+
+function ShowDeatils(){
+    alert('feature added soon')
+}
